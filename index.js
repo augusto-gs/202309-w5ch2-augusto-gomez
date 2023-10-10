de2f12e31458c9cc83d26505eb19d4e169c07f7d
augusto-gs <agomezsantos95@gmail.com>
2023-10-10 20:58:42 +0200

    Add stricEquals function

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log("Hellos");
+const strictEquals = (valueA, valueB) => {
+  if (Object.is(NaN, valueA)) {
+    return false;
+  }
+
+  if (Object.is(0, valueA)) {
+    return false;
+  }
+
+  if (Object.is(-0, valueA)) {
+    return false;
+  }
+
+  const isValueEqual = Object.is(valueA, valueB);
+  return isValueEqual;
+};
+
+console.log(strictEquals(3, 3));
